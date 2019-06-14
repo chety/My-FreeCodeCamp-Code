e1c7b9df56b2be23c7af9a55689c9f72d7d2cd14
Chety <cetincelik20@gmail.com>
2019-06-14 16:04:53 +0300

    Update 21 Bonfire Arguments Optional.js

--- Intermediate Algorithm Scripting/21 Bonfire Arguments Optional.js
+++ Intermediate Algorithm Scripting/21 Bonfire Arguments Optional.js
@@ -21,47 +21,17 @@ rafase282@gmail.com
 http://www.freecodecamp.com/rafase282
 */
 
-function add() {
-  // Function to check if a number is actually a number
-  // and return undefined otherwise.
-  var checkNum = function(num) {
-    if (typeof num !== 'number') {
-      return undefined;
-    } else
-      return num;
-  };
-
-  // Check if we have two parameters, check if they are numbers
-  // handle the case where one is not
-  // returns the addition.
-  if (arguments.length > 1) {
-    var a = checkNum(arguments[0]);
-    var b = checkNum(arguments[1]);
-    if (a === undefined || b === undefined) {
-      return undefined;
-    } else {
-      return a + b;
-    }
-  } else {
-    // If only one parameter was found, returns a new function that expects two
-    // Store first argument before entering the new function scope
-    var c = arguments[0];
-
-    // Check the number again, must be outside the function to about returning an object
-    // instead of undefined.
-    if (checkNum(c)) {
-      // Return function that expect a second argument.
-      return function(arg2) {
-        // Check for non-numbers
-        if (c === undefined || checkNum(arg2) === undefined) {
-          return undefined;
-        } else {
-          // if numbers then add them.
-          return c + arg2;
-        }
-      };
+function add(...args) {
+  const length = args.length;
+  if (length === 1 && typeof args[0] === "number") {
+    return function (secondNumber) {
+      return add(args[0], secondNumber);
     }
   }
+  if (length === 2 && typeof args[0] === "number" && typeof args[1] === "number") {
+    return args[0] + args[1];
+  }
+
 }
 
 add(2, 3);
